cdd051fea340636fb7b67b40635095cd69f8b68c
Tom Larrow <skraut@gmail.com>
2017-07-21 21:56:46 -0400

    have some ball movement

--- scenes/ball/Ball.gd
+++ scenes/ball/Ball.gd
@@ -1,4 +1,19 @@
-extends RigidBody2D
+extends KinematicBody2D
+
+var speed = 200
+var vel = Vector2()
 
 func _ready():
-	apply_impulse(Vector2(1,1),Vector2(1,1))
+	print("ready")
+	randomize()
+	vel = Vector2(rand_range(-1,1), rand_range(-1,1)).normalized() * speed 
+	print(vel)
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	var motion = move(vel * delta)  # Move the Body
+	if is_colliding():
+		var n = get_collision_normal()
+		motion = n.reflect(motion)
+		vel = n.reflect(vel)
+		move(motion)
