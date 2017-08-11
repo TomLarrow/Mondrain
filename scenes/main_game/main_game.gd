48ecc7af726cedce3106f216768c228eef764ddb
Tom Larrow <tomlarrow@gmail.com>
2017-08-10 22:51:28 -0400

    Adding some mouse cursors, and setting one at the start of the game

---
+++ scenes/main_game/main_game.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var mouse_horizontal = preload("res://scenes/main_game/mouse_horizontal.png")
+var mouse_vertical = preload("res://scenes/main_game/mouse_vertical.png")
+
+
+func _ready():
+	Input.set_custom_mouse_cursor(mouse_horizontal)
+	
