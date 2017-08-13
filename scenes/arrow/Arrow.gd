840ba32b238cc3832ef688a52882e8b3939e4c39
Tom Larrow <tomlarrow@gmail.com>
2017-08-12 20:12:13 -0400

    RayCasting each direction, next I need to sort distance

--- scenes/arrow/Arrow.gd
+++ scenes/arrow/Arrow.gd
@@ -1,4 +1,6 @@
-extends Sprite
+extends Node2D
+
+onready var rays = [ get_node("ray_down"), get_node("ray_up"), get_node("ray_left"), get_node("ray_right") ]
 
 func _ready():
 	set_fixed_process(true)
@@ -6,4 +8,5 @@ func _ready():
 
 func _fixed_process(delta):
 	set_pos(get_global_mouse_pos())
-	
+	for i in range(0, rays.size()):
+		print(rays[i].get_collision_point())
