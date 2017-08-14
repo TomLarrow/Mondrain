facb4480a542bdb53903e1433fa97dffab741bfd
Tom Larrow <tomlarrow@gmail.com>
2017-08-14 12:03:45 -0400

    Reworking the arrow, and adding some signals to start making walls

--- scenes/arrow/Arrow.gd
+++ scenes/arrow/Arrow.gd
@@ -1,24 +1,31 @@
 extends Node2D
+signal draw_wall
 
-onready var rays = [ get_node("ray_left"), get_node("ray_down"), get_node("ray_right"), get_node("ray_up") ]
+onready var rays = [ get_node("ray_up"), get_node("ray_right"), get_node("ray_down"), get_node("ray_left") ]
 onready var sprite = get_node("Sprite")
 var direction = null
 
 func _ready():
 	set_fixed_process(true)
-	pass
-
+	set_process_input(true)
+ 
 func _fixed_process(delta):
 	set_pos(get_global_mouse_pos())
 	direction = find_shortest_ray()
-	sprite.set_rotd( 90 * direction)
+	sprite.set_rotd( 360 - (90 * direction)) # since we want the arrow to point AWAY from the ray, reverse the rotation
+
+func _input(event):
+	# if user left clicks
+	if(event.type == InputEvent.MOUSE_BUTTON):
+		if(event.button_index == 1):
+			emit_signal("draw_wall", direction, rays[direction].get_collision_point())
 
 func find_shortest_ray():
 	var smallest = 100000.0
-	var dir = 0
+	var shortest_ray = 0
 	for i in range (0, rays.size()):
 		var distance_to_wall = rays[i].get_collision_point().distance_to(get_global_pos())
 		if ( distance_to_wall < smallest ):
 			smallest = distance_to_wall
-			dir = i
-	return dir
+			shortest_ray = i
+	return shortest_ray
--- scenes/main_game/main_game.gd
+++ scenes/main_game/main_game.gd
@@ -1,4 +1,5 @@
 extends Node
+signal draw_wall
 
 # class member variables go here, for example:
 # var a = 2
@@ -10,4 +11,8 @@ var mouse_vertical = preload("res://scenes/main_game/mouse_vertical.png")
 
 func _ready():
 	#Input.set_custom_mouse_cursor(mouse_horizontal)
+	find_node("Arrow").connect("draw_wall", self, "_on_draw_wall")
 	pass
+
+func _on_draw_wall(direction, wall_origin):
+	print("I'm drawing a wall to direction ", direction, " from ", wall_origin)
